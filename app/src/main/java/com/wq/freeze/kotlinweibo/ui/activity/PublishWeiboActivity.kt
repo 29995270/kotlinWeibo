0a7bfba384cdad2416acd00dcaa93dc8f29a4626
29995270 <wangqi@kanjian.com>
2016-03-08 18:31:18 +0800

    aa

--- app/src/main/java/com/wq/freeze/kotlinweibo/ui/activity/PublishWeiboActivity.kt
+++ app/src/main/java/com/wq/freeze/kotlinweibo/ui/activity/PublishWeiboActivity.kt
@@ -1,11 +1,8 @@
 package com.wq.freeze.kotlinweibo.ui.activity
 
-import android.graphics.Color
 import android.os.Bundle
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
-import com.wq.freeze.kotlinweibo.extension.aaaLoge
-import org.jetbrains.anko.backgroundColor
-import org.jetbrains.anko.verticalLayout
+import com.wq.freeze.kotlinweibo.R
 
 /**
  * Created by wangqi on 2016/3/3.
@@ -14,6 +11,6 @@ class PublishWeiboActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_publish)
     }
 }
